482e64aa12846a37469eb3903fb8cee645062d36
Radwa99 <radwa.prof.9@gmail.com>
2023-04-13 14:40:37 +0200

    slide test

--- lab2/src/app/Components/second/second.component.ts
+++ lab2/src/app/Components/second/second.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./second.component.css']
 })
 export class SecondComponent {
+  slideIndex = 1;
 
+  ngOnInit() {
+    this.showSlide(this.slideIndex);
+  }
+
+  nextSlide() {
+    this.showSlide(this.slideIndex += 1);
+  }
+
+  previousSlide() {
+    this.showSlide(this.slideIndex -= 1);
+  }
+
+  showSlide(n:any) {
+    let i;
+    const slides = document.getElementsByTagName('img');
+    if (n > slides.length) {this.slideIndex = 1;}
+    if (n < 1) {this.slideIndex = slides.length;}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    slides[this.slideIndex-1].style.display = 'block';
+  }
 }
