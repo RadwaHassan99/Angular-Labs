eaf08750b3798806d37a172f6a0192373ed059d1
Radwa99 <radwa.prof.9@gmail.com>
2023-04-13 13:16:21 +0200

    lab2-part1

--- lab2/src/app/app.module.ts
+++ lab2/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './Components/first/first.component';
+import { SecondComponent } from './Components/second/second.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
